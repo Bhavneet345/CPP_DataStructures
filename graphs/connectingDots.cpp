bd8d641465fc1233a6840485f17ae85e7fe5c427
Bhavneet Singh <84433782+Bhavneet345@users.noreply.github.com>
2022-02-11 12:49:02 +0530

    Update connectingDots.cpp

--- graphs/connectingDots.cpp
+++ graphs/connectingDots.cpp
@@ -2,7 +2,7 @@
 #include<vector>
 using namespace std;
 
-bool helper(vector<vector<char>> &board, vector<vector<bool> > &visited, int i, int j, int n, int m, char element)
+bool helper(vector<vector<char> > &board, vector<vector<bool> > &visited, int i, int j, int n, int m, int startx, int starty, char element)
 {
     if(i < 0 || i >= n || j < 0 || j >= m || board[i][j] != element)
     {
@@ -13,29 +13,32 @@ bool helper(vector<vector<char>> &board, vector<vector<bool> > &visited, int i, 
         return true;
     }
     visited[i][j] = true;
-    board[i][j] = '.';
-    bool result = helper(board, visited, i - 1, j, n, m, element);
-    if(!result){
-        result = helper(board, visited, i + 1, j, n, m, element);
+    int dxdy[][4] = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+    for(int k = 0; k < 4; k++)
+    {
+        int newx = i + dxdy[k][0];
+        int newy = j + dxdy[k][1];
+        if(newx == startx && newy == starty)
+        {
+            continue;
+        }
+        bool res = helper(board, visited, newx, newy, n, m, i, j, element);
+        if(res == true)
+        {
+            return res;
+        }
     }
-    if(!result){
-        result = helper(board, visited, i, j - 1, n, m, element);
-    }
-    if(!result){
-        result = helper(board, visited, i, j + 1, n, m, element);
-    }
-    board[i][j] = element;
-    return result;
+    return false;
 }
 
-bool hasCycle(vector<vector<char>> &board, int n, int m)
+bool hasCycle(vector<vector<char> > &board, int n, int m)
 {
     vector<vector<bool> >visisted(n, vector<bool>(m, false));
     for(int i = 0; i < n; i++)
     {
         for(int j = 0; j < m; j++)
         {
-            if(!visisted[i][j] && helper(board, visisted, i, j, n, m, board[i][j])){
+            if(!visisted[i][j] && helper(board, visisted, i, j, n, m, i, j, board[i][j])){
                 return true;
             }
         }
@@ -47,7 +50,7 @@ int main() {
     int n, m;
     cin >> n >> m;
 
-    vector<vector<char>> board(n, vector<char>(m));
+    vector<vector<char> > board(n, vector<char>(m));
 
     for (int i = 0; i < n; ++i) {
         for (int j = 0; j < m; ++j) {
