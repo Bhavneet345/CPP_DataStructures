48a488b05a617885641ede8c69c64bdc26254e0d
Bhavneet Singh <84433782+Bhavneet345@users.noreply.github.com>
2023-04-14 11:06:12 +0530

    Add files via upload

---
+++ Arrays/array_declaration.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+
+//different array initialization
+
+int main()
+{
+    int arr[10] = {0}; //declares an array of size 10 with all zeros.
+    int arr1[10] = {1, 2, 3, 4}; //places as per specification.
+    int arr[100] = {100}; //places 100 at only 1st position.
+}
